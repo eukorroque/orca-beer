1779c31c83c3199990e969bc3b87828f08fce4eb
Debora Almeida <deboraalmeida89@gmail.com>
2023-05-06 19:11:20 -0300

    Add categoriaProduto e unidadeProduto model

--- src/routes/produto.routes.ts
+++ src/routes/produto.routes.ts
@@ -9,7 +9,5 @@ const router = Router()
 
 router
   .get('/produtos', Controller.getAll.bind(Controller))
-  //.post('/usuario/:type', Controller.create.bind(Controller))
-  //.put('/usuario/:id', Controller.update.bind(Controller))
 
 export default router
