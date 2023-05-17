518d4c53889eb7ad83e4e90791a41cc2bff7e85b
Flaviano Rodrigues <flavianodev@gmail.com>
2023-05-17 11:57:46 -0300

    componentizando o header to lojista

--- mobile/src/controllers/HomeLojistaController/index.tsx
+++ mobile/src/controllers/HomeLojistaController/index.tsx
@@ -2,21 +2,17 @@
 // Arquivo criado: 16/05/2023 às 11:05
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import React from 'react'
-import {Image} from 'react-native'
-import SelectDropdown from 'react-native-select-dropdown'
-import TextDefault from '../../components/TextDefault'
 import { HomeLogistaControllerParamList } from '../../types/HomeLogistaControllerParamList'
 import theme from '../../config/theme'
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
+import HeaderLojista from '../../components/HeaderLojista'
 import PreferenciasLojistaScreen from '../../screens/PreferenciasLojistaScreen'
 import HomeLogistaScreen from '../../screens/HomeLojistaScreen'
-import data from './data.json'
-import * as S from './styles'
 
 const Tab = createBottomTabNavigator<HomeLogistaControllerParamList>()
 
 const HomeLogistaController = () => {
-  
+
   return (
     <Tab.Navigator
       initialRouteName='Home'
@@ -25,49 +21,11 @@ const HomeLogistaController = () => {
           backgroundColor: theme.colors.primary,
           height: 180,
         },
-        headerTitle: () => (
-          <S.HeaderContainer>
-            <S.ProfileContainer>
-              <Image source={require('./Profile-PNG-File.png')} style={{width: 66, height: 66 }}/>
-              <S.ProfileContainer>
-                <TextDefault bold>Hello World! Fine Drinks by Debora Almeida!</TextDefault>
-              </S.ProfileContainer>
-            </S.ProfileContainer>
-            <S.IconsContainer>
-              <FontAwesome name={'comments'} color={'#000'} size={25} />
-              <FontAwesome name={'bell'} color={'#000'} size={25} />
-            </S.IconsContainer>
-            <S.AdressContainer>            
-              <SelectDropdown
-                dropdownStyle={{width: 300, borderRadius: 16}}
-                buttonStyle={{width: 300, borderRadius: 16, backgroundColor: theme.colors.primary}}
-                buttonTextStyle={{fontFamily: theme.fonts.regular, fontSize: theme.fontSizes.body.p3}}
-                rowTextStyle={{fontFamily: theme.fonts.regular}}
-                data={data.enderecos}
-                onSelect={(selectedItem, index) => {
-                  console.log(selectedItem, index)
-                }}
-                buttonTextAfterSelection={(selectedItem) => {
-                  return `${selectedItem.rua}, ${selectedItem.numero}, ${selectedItem.cidade}, ${selectedItem.estado}`
-                  
-                }}
-                rowTextForSelection={(item) => {
-                  return `${item.rua}, ${item.numero}`
-                }}
-                showsVerticalScrollIndicator={true}
-                renderDropdownIcon={isOpened => {
-                  return <FontAwesome name={isOpened ? 'chevron-up' : 'chevron-down'} color={'#000'} size={14} />;
-                }}
-                defaultButtonText='Selecione o seu endereço'
-              />
-            </S.AdressContainer>
-           
-          </S.HeaderContainer>
-        ),
         tabBarStyle: {
           backgroundColor: theme.colors.background,
           height: 60,
         },
+        headerTitle: () => <HeaderLojista />,
         tabBarActiveTintColor: theme.colors.mainText,
         tabBarInactiveTintColor: '#ccc',
         tabBarLabelStyle: {
