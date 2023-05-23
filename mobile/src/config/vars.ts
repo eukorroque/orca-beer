56a84b9a96a0cdade779572de5adc3359e8632e5
Flaviano Rodrigues <flavianodev@gmail.com>
2023-05-23 16:04:32 -0300

    ajustando alguns detalhes para uma melhor experiancia na hora da apresentação

--- mobile/src/config/vars.ts
+++ mobile/src/config/vars.ts
@@ -1,3 +1,4 @@
 export default {
-  host: '192.168.1.13'
+  host: '10.0.0.117',
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OSwic3RhdHVzSWQiOjgsInRwQ29udGEiOjIsIm5vbWUiOiJKb8OjbyBHb21lcyIsImlhdCI6MTY4NDcwOTIyOSwiZXhwIjoxNjg1MzE0MDI5fQ.rStvYgS5RF6-HsYJy1Cwh4gkIbcd-P14kGyeT7DF-pA'
 }
